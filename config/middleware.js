3435b2eb5df51b1ca32add72ea24d509f7c7bbc0
pranavhajare <hajare.pranav@gmail.com>
2023-11-20 16:56:05 +0530

    created student and employee model

--- config/middleware.js
+++ config/middleware.js
@@ -33,7 +33,17 @@ module.exports.createFolders = async (req, res, next) => {
 
 		if (!fs.existsSync(directory)) fs.mkdirSync(directory);
 
+		directory = path.join(__dirname, "..", "/uploads/employees");
+		if (!fs.existsSync(directory)) fs.mkdirSync(directory);
 
+		directory = path.join(__dirname, "..", "/uploads/employees/avatars");
+		if (!fs.existsSync(directory)) fs.mkdirSync(directory);
+
+		directory = path.join(__dirname, "..", "/uploads/students");
+		if (!fs.existsSync(directory)) fs.mkdirSync(directory);
+
+		directory = path.join(__dirname, "..", "/uploads/students/avatars");
+		if (!fs.existsSync(directory)) fs.mkdirSync(directory);
 
 
 		directory = path.join(__dirname, "..", "/workers");
@@ -48,4 +58,36 @@ module.exports.createFolders = async (req, res, next) => {
 
 	next();
 
+};
+
+
+//Handles the MongoDB Validation Errors
+module.exports.DBValidation = (req, res, err) => {
+	let error = "Error";
+	let code = "Unknown";
+	let index = 0;
+
+	if (!err.code) {
+		index = err.message.lastIndexOf(":");
+		error = err.message.substr(index + 2);
+		code = "No Code";
+	} else {
+		index = err.message.indexOf(":");
+		error = err.message.substr(0, index);
+		code = err.code;
+	}
+
+	let obj = {
+		type: "Database",
+		name: err.name,
+		message: error,
+		code: code,
+	};
+	console.log(obj);
+
+	//Return Format
+	// const { DBValidation } = require("./config/middleware");
+	// return DBValidation(req, res, err);
+
+	return res.json(obj);
 };
