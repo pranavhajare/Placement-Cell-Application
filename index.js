2019e7f68bbbe74de9ab8dc559e97e72326882c7
pranavhajare <hajare.pranav@gmail.com>
2023-11-20 18:05:16 +0530

    setting up passportJS and Passport local strategy

--- index.js
+++ index.js
@@ -24,6 +24,16 @@ const cors = require("cors");
 //Require the Morgan Module for Logging
 const logger = require("morgan");
 
+//Requires the Cookie-Parser Module
+const cookieParser = require("cookie-parser");
+//Requires the Express-Session Module for the Session Cookie
+const session = require("express-session");
+//Requires the MongoStore
+const MongoStore = require("connect-mongo");
+//Requires the PassportJS Module for the Authentication
+const passport = require("passport");
+
+
 //Require the View Helpers
 const viewHelpers = require("./config/view-helpers")(app);
 
@@ -36,6 +46,9 @@ const route = require("./routes/index");
 
 //Requires the Custom Middleware
 const customMiddleware = require("./config/middleware");
+
+//Requires the Passport Local Strategy used for the Authentication
+const passportLocal = require("./config/passport-local-strategy");
 
 //Middleware - CORS
 app.use(cors());
@@ -74,8 +87,17 @@ const expressLayouts = require("express-ejs-layouts");
 //Requires the EJS Module
 const ejs = require("ejs");
 
+//Middleware - Cookie Parser for accessing & parsing the cookies
+app.use(cookieParser("placementcellApp"));
+
 //Middleware - Express App uses Static Files in the Assets Folder
 app.use(express.static(env.asset_path));
+
+//Middleware - Make the '/storage' path available to the browser
+app.use("/storage", express.static(__dirname + "/storage"));
+//Middleware - Make the '/uploads' path available to the browser
+app.use("/uploads", express.static(__dirname + "/uploads"));
+
 //Middleware - Express App uses expressLayouts to tell that the views which are going to be rendered belongs to some layout.
 app.use(expressLayouts);
 
@@ -93,6 +115,48 @@ app.set("view engine", "ejs");
 
 //Set Up - Template Engine Views Folder Path (..../views)
 app.set("views", path.join(__dirname, "views"));
+
+//Middleware - Creates a Session Cookie and Encrypts it with a Key
+app.use(
+	session({
+		//Cookie Name
+		name: "PlacementCellApplication",
+		//Secret Key for encrypting the session cookie
+		secret: env.session_cookie_key,
+		//Don't save the uninitialized session
+		saveUninitialized: false,
+		//Don't re-save the session if it is not modified
+		resave: false,
+		//Cookie Options
+		cookie: {
+			//Cookie Expiry Time - 100 Minutes
+			maxAge: 1000 * 60 * 100,
+		},
+		//MongoStore is used to store the Session Cookies in the MongoDB
+		store: MongoStore.create(
+			{
+				//DB Connection URL
+				mongoUrl: `mongodb://0.0.0.0:27017/placementdb`,
+				//Interacts with the mongoose to connect to the MongoDB
+				mongooseConnection: db,
+				//To auto remove the store
+				autoRemove: "disabled",
+			},
+			(err) => {
+				//If there is an error
+				if (err) {
+					console.log(err || "connect-mongodb setup ok");
+				}
+			}
+		),
+	})
+);
+//Middleware - Initializes & uses PassportJS for the Authentication
+app.use(passport.initialize());
+//Middleware - PassportJS creates & maintains the Session
+app.use(passport.session());
+//Middleware - Sets the Authenticated User in the Response
+app.use(passport.setAuthenticatedUser);
 
 //Middleware - Creates Folders if they don't exist
 app.use(customMiddleware.createFolders);
